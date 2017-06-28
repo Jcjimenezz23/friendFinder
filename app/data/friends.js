5d91bb008f9cb0f22bd5183a2d4d09fa2ecbe38d
Jc <jcjimenezz23@gmail.com>
2017-06-28 17:25:39 -0400

    added friends.js data

---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+var friendList = [
+	{
+	"name":"Primo",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+      9,
+      1,
+      4,
+      4,
+      5,
+      4,
+      2,
+      5,
+      8,
+      1
+    ]
+	},
+	{
+		 "name":"Loco",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      2,
+      1,
+      5,
+      4,
+      5,
+      1,
+      3,
+      5,
+      9,
+      1
+    ]
+	}
+]
+
+module.exports = friendList;
